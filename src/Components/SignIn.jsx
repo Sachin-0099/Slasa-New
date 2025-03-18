83eb40dbe95b187caf202f6538f5b87cb1dd7bac
Sachin Kumar Singh <sachinkumarsingh@Sachins-MacBook-Air.local>
2025-03-18 12:22:53 +0530

    changes in signin" "

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 
 
 const SignIn = () => {
