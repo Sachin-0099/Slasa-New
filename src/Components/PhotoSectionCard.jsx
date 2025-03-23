ee71f88e020f9b09cd4272d110a57679e750c3d2
Sachin Kumar Singh <sachinkumarsingh@Sachins-MacBook-Air.local>
2025-03-23 21:39:40 +0530

    spacing

--- src/Components/PhotoSectionCard.jsx
+++ src/Components/PhotoSectionCard.jsx
@@ -4,31 +4,34 @@ const PhotoSectionCard = ({ PhotoData, title, width, Description }) => {
   return (
     <div className='h-full w-screen lg:mb-10 md:mb-5 mb-2 flex flex-col items-center'>
       <h1 className='relative text-transparent bg-gradient-to-r from-[#3087D1] via-[#3087D5] to-black bg-clip-text font-bold lg:text-6xl md:text-4xl text-3xl text-center font-semibold hover:underline hover:underline-offset-8 hover:decoration-[#3087D1]'>
-        <span className={`hidden lg:inline-block absolute bottom-0 lg:w-[${width}] h-[4px] bg-gradient-to-r from-[#3087D1] via-[#3087D5] to-black mt-[20vh]`}></span>
         {title}
       </h1>
+      
       {Description.map((desc, index) => (
         <p key={index} className='lg:px-10 lg:mx-50 md:mx-30 lg:py-5 py-2 mx-5 text-gray-500 md:text-xl text-sm lg:text-xl text-center'>
           {desc.description}
         </p>
       ))}
+      
       <div className='h-full w-full flex items-center justify-center'>
-        <div className='h-[80%] lg:w-[70%] md:w-[70%] w-[90%] mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center '>
+        <div className='h-[80%] lg:w-[70%] md:w-[70%] w-[90%] mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center'>
           {PhotoData.map((photo, index) => (
-            <div key={index} className='lg:h-[30vh] lg:w-[40vh] md:h-[25vh] md:w-[40vh] h-[25vh] w-[22vh] flex justify-center items-center '>
+            <div key={index} className='lg:h-[30vh] lg:w-[40vh] md:h-[25vh] md:w-[40vh] h-[25vh] w-[22vh] flex justify-center items-center'>
               <img src={photo.src} className='h-full w-full object-cover' alt='Photo' />
             </div>
           ))}
         </div>
       </div>
+
+      {/* Adjusted Instagram link placement */}
       {Description.map((des, index) => (
-        <div key={index} className='w-[40%] h-[10%] flex lg:py-10 md:py-12 mt-10 items-center  justify-center mt-[-50] mb-10'>
-          <img src={des.icon} alt='Icon' className='lg:w-[10vh] md:w-[8vh] w-[5vh] '  />
+        <div key={index} className='w-full flex flex-row items-center justify-center mt-10 mb-10'>
+          <img src={des.icon} alt='Icon' className='lg:w-[10vh] md:w-[8vh] w-[5vh] mb-3' />
           <a
             href={des.link.startsWith('http') ? des.link : `https://${des.link}`}
             target='_blank'
             rel='noopener noreferrer'
-            className='hover:text-cyan-600 text-cyan-500 px-5 lg:text-2xl md:text-2xl text-md'
+            className='hover:text-cyan-600 text-cyan-500 px-5 lg:text-2xl md:text-2xl text-md text-center'
           >
             {des.link.replace('https://', '')}
           </a>
