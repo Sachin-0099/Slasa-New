d71acd41f32495934856c104b739c183a3ea596e
Sachin Kumar Singh <sachinkumarsingh@Sachins-MacBook-Air.local>
2025-03-25 23:59:40 +0530

    HeaderMain border added

--- src/Components/HeaderMain.jsx
+++ src/Components/HeaderMain.jsx
@@ -7,20 +7,20 @@ const HeaderMain = () => {
       <nav className="navbar bg-white p-0 border-l-2">
         <div className="flex justify-start space-x-4 py-2 overflow-x-auto no-scrollbar">
           {/* Category 1 */}
-          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-4 py-2">
-            <a href="#" className="text-black text-base sm:text-lg">Acrylic Accessories</a>
+          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-3 py-1">
+            <a href="#" className="text-black text-base sm:text-sm">Acrylic Accessories</a>
           </div>
           {/* Category 2 */}
-          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-4 py-2">
-            <a href="#" className="text-black text-base sm:text-lg">Shop</a>
+          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-3 py-1">
+            <a href="#" className="text-black text-base sm:text-sm">Shop</a>
           </div>
           {/* Category 3 */}
-          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-4 py-2">
-            <a href="#" className="text-black text-base sm:text-lg">Photography & Videography</a>
+          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-3 py-1">
+            <a href="#" className="text-black text-base sm:text-sm">Photography & Videography</a>
           </div>
           {/* Category 4 */}
-          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-4 py-2">
-            <a href="#" className="text-black text-base sm:text-lg">Other Services</a>
+          <div className="flex-shrink-0 border-2 border-gray-500 rounded-md px-3 py-1">
+            <a href="#" className="text-black text-base sm:text-sm">Other Services</a>
           </div>
           {/* Add more categories if necessary */}
         </div>
